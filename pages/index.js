21fa1025a15a4126450469af603e7fb4fd8ac198
mood <mooddddd@naver.com>
2023-08-21 18:29:16 +0900

    section 24 : 381 main page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
-export default function MainPage() {
-  return <h1>Hello, Next.js!</h1>;
-}
+import MeetupList from '@/components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'a frist meet',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Prague_from_Powder_Tower_01.jpg/2560px-Prague_from_Powder_Tower_01.jpg',
+    address: '서울특별시',
+    description: '여기서 만나용',
+  },
+  {
+    id: 'm2',
+    title: 'a second meet',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Prague_from_Powder_Tower_01.jpg/2560px-Prague_from_Powder_Tower_01.jpg',
+    address: '경기도',
+    description: '저기서 만나용~~~~',
+  },
+];
+
+const HomePage = () => {
+  return (
+    <>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </>
+  );
+};
+
+export default HomePage;
